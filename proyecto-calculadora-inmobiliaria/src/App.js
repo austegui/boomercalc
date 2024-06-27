6ae29d6f41e0b801b0e5683d393c7164b1661e94
austegui <gustavo.villarreal@gmail.com>
2024-06-26 22:01:56 -0500

    Create App.js

---
+++ proyecto-calculadora-inmobiliaria/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CalculadoraPerfilFinanciero from './components/CalculadoraPerfilFinanciero';
+
+function App() {
+  return (
+    <div className="App">
+      <CalculadoraPerfilFinanciero />
+    </div>
+  );
+}
+
+export default App;
